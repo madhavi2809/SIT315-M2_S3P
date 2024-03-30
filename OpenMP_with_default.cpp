c56d9751bb5c3b76acab3f5fa793c5a098dbb189
madhavi2809 <115390405+madhavi2809@users.noreply.github.com>
2024-03-30 20:50:52 +0530

    Update OpenMP_with_default.cpp

--- OpenMP_with_default.cpp
+++ OpenMP_with_default.cpp
@@ -8,38 +8,39 @@ using namespace std::chrono;
 using namespace std;
 
 void randomVector(int vector[], int size) {
-  for (int i = 0; i < size; i++) {
-    vector[i] = rand() % 100;
-  }
+    for (int i = 0; i < size; i++) {
+        vector[i] = rand() % 100;
+    }
 }
 
 int main() {
-  unsigned long size = 100000000;
-  srand(time(0));
-  int *v1, *v2, *v3;
-  v1 = (int *)malloc(size * sizeof(int));
-  v2 = (int *)malloc(size * sizeof(int));
-  v3 = (int *)malloc(size * sizeof(int));
-  randomVector(v1, size);
-  randomVector(v2, size);
+    unsigned long size = 100000000;
+    srand(time(0));
+    int* v1, *v2, *v3;
+    v1 = (int*)malloc(size * sizeof(int));
+    v2 = (int*)malloc(size * sizeof(int));
+    v3 = (int*)malloc(size * sizeof(int));
+    randomVector(v1, size);
+    randomVector(v2, size);
 
-  auto start = high_resolution_clock::now();
-  
-  #pragma omp parallel default(none) shared(v1, v2, v3, size)
-  {
-      #pragma omp parallel for
-      for (int i = 0; i < size; i++) {
+    auto start = high_resolution_clock::now();
+
+    int total = 0; // Initialize total outside the parallel region
+
+    #pragma omp parallel for default(none) shared(v1, v2, v3, size) reduction(+:total)
+    for (int i = 0; i < size; i++) {
         v3[i] = v1[i] + v2[i];
-      }
-  }
+        total += v3[i]; // Update total using reduction clause
+    }
 
-  auto stop = high_resolution_clock::now();
-  auto duration = duration_cast<microseconds>(stop - start);
-  cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
+    cout << "Total sum: " << total << endl; // Print the total sum
 
-  free(v1);
-  free(v2);
-  free(v3);
+    free(v1);
+    free(v2);
+    free(v3);
 
-  return 0;
+    return 0;
 }
